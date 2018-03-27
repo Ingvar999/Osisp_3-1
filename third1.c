1f5208454f90dc1e23457d1ee5809c6bfbdb7bba
Ingvar <shymanski999@gmail.com>
2018-03-28 02:07:58 +0300

    Init

--- third1.c
+++ third1.c
@@ -8,7 +8,7 @@
 
 int main(int argc, char *argv[])
 {
-	int n = atoi(argv[1]), i = atoi(argv[2]), j = atoi(argv[3]), N = atoi(argv[4]), pid;
+	int n = atoi(argv[1]), i = atoi(argv[2]), j = atoi(argv[3]), N = atoi(argv[4]), pid;//это идентификатор процесса (число)
 	
 	int k = j*2 + 1;
 	int sign = (j & 1) * (-2) + 1;
@@ -27,7 +27,7 @@ int main(int argc, char *argv[])
 	
 	float result = sign * pow / fact;
 
-	printf("%d / %d / %d / %.4f\n", pid = getpid(), i, j, result);
+	printf("%d / %d / %d / %.4f\n", pid = getpid(), i, j, result); //идентификатор процесса, номер ряда Тейлора, номер члена ряда 
 	FILE *f = fopen("temp~", "r+");
 	fseek(f, (i*n + j)*sizeof(float), SEEK_SET);
 	fwrite(&result, sizeof(float), 1, f);
